e5770f749cad0bc683912f5c0c6378d63a6f1e4f
Alishba-Khan02 <ak6445268@gmail.com>
2024-04-21 15:14:04 +0500

    First JS Alert

---
+++ app.js
@@ -0,0 +1,9 @@
+alert("Hello Connections!");
+
+alert("Let's get connected to start coding");
+
+alert("Welcome to JS Land...\nHappy Coding!");
+
+alert("Keep Coding!\nPrevent this page from creating additional dialogs")
+
+alert("Hello...I can run JS through my web browser's console");
